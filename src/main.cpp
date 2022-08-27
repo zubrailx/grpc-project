92fddd5a506ab15ca7ff1218d5bd8cd59a7e339c
Nikita Kulakov <Nikitka.Kulakov@yandex.ru>
2022-08-27 21:45:46 +1000

    Add example project

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,18 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-template <typename T>
+template<typename T>
 void print(T value) {
-  cout << __PRETTY_FUNCTION__ << endl;
-  cout << value << endl;
+	cout << __PRETTY_FUNCTION__ << endl;
+	cout << value << endl;
 }
 
-template <typename T, typename... Args>
+template<typename T, typename... Args>
 void print(T value, Args... args) {
-  cout << __PRETTY_FUNCTION__ << endl;
-  cout << value << endl;
-  print(args...);
+	cout << __PRETTY_FUNCTION__ << endl;
+	cout << value << endl;
+	print(args...);
 }
 
-int main() {
-  print("abcde", 101, 101.4, true, 4, 91);
-}
+int main() { print("abcde", 101, 101.4, true, 4, 91); }
